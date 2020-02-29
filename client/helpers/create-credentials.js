73fffbabcbad4b58011ee8236c5f71a368bf7cba
Denis Zavgorodny <deniszavgorodny@MacBook-Pro-Denis.local>
2020-02-29 22:57:43 +0200

    update demo UI

--- client/helpers/create-credentials.js
+++ client/helpers/create-credentials.js
@@ -15,10 +15,10 @@ const createCredentialDefaultArgs = (name, username, id, challenge) => ({
 
       pubKeyCredParams: [{alg: -7, type: "public-key"}],
       authenticatorSelection: {
-          authenticatorAttachment: "cross-platform", //platform
+        //   authenticatorAttachment: "cross-platform", //platform
           // Try to use UV if possible. This is also the default.
-          userVerification: "required",
-          requireResidentKey: true,
+        //   userVerification: "required",
+          //requireResidentKey: true,
       },
       timeout: 60000,
       challenge: Uint8Array.from(challenge, c => c.charCodeAt(0)),
